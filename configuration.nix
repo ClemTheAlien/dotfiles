fbd123ec0e2a84b0977d6090fba06a604c57d3ee
ClemTheAlien <ClemTheAlien@proton.me>
2025-02-17 21:15:24 -0600

    Manual Mirror #2

--- configuration.nix
+++ configuration.nix
@@ -10,31 +10,15 @@
 	#Update Kernel (fix audio)
 	boot.kernelPackages = pkgs.linuxPackages_latest;
  	# Bootloader.
-	boot.loader = {
-		grub = {
-			enable = true;
-			useOSProber = true;
-			device = "nodev";
-			efiSupport = true;
-		};
-		efi = {
-			canTouchEfiVariables = true;
-			efiSysMountPoint = "/boot/efi";
-		};
-	};
+	boot.loader.systemd-boot.enable =true;
 	# Hostname
 	networking.hostName = "CHANGE ME";
-
-
 	# Enable networking
 	networking.networkmanager.enable = true;
-
   	#Timezone
 	time.timeZone = "CHANGE ME";
-
 	# Select internationalisation properties.
 	i18n.defaultLocale = "en_US.UTF-8";
-
 	i18n.extraLocaleSettings = {
 		LC_ADDRESS = "en_US.UTF-8";
 		LC_IDENTIFICATION = "en_US.UTF-8";
@@ -46,7 +30,6 @@
 		LC_TELEPHONE = "en_US.UTF-8";
 		LC_TIME = "en_US.UTF-8";
 	};
-
 	# Define a user account.
 	users.users.clemmie = {
 		isNormalUser = true;
@@ -54,38 +37,62 @@
 		extraGroups = [ "networkmanager" "wheel" ];
 		packages = with pkgs; [];
 	};
+	#Allow Non-Foss
+	nixpkgs.config.allowUnfree = true;	
 	# Packages
 	environment.systemPackages = with pkgs; [
-		alacritty #terminal
-		rofi #launcher
-		brave #browser
-		fastfetch #fetch
-		pcmanfm #file explorer
-		git # ver control
-		krita #picture manipulation tool
-		obs-studio #video recording software
-		wireshark # packet sniffing 
-		keepassxc # password manager
-		libreoffice #document suite editor
+		# System Tools
 		bluez #bluetooth
 		bluez-tools #bluetooth
-		vlc #video 
-		prismlauncher #minecraft
-		vesktop #discord
-		handbrake #video compressor
-		curtail #image compressor
-		kicad #pcb maker
-		metadata-cleaner #cleans metadata
-		logisim #pcb maker
+		os-prober #dual boot
+
+		# Desktop Env Tools 
+		alacritty #terminal
+		rofi #launcher
+		fastfetch #fetch
+		pcmanfm #file explorer
 		picom #compositor
 		nitrogen #background handler
 		shutter #screenshotter
 		pywal #colorscheme generator
 		networkmanagerapplet #wifi gui
 		blueman #bluetooth gui
-		os-prober #dual boot
 		pavucontrol #audio applet
-		emacs #for coding, org mode, etc...
+		feh # to view images 
+		# Apps
+			# General Apps 
+		librewolf #browser
+		libreoffice #document suite editor
+		keepassxc # password manager
+		vlc #video 
+
+			# Chat Apps
+		vesktop #discord
+		weechat #irc
+
+		#Coding Tools
+		git # ver control
+		vscodium #IDE 
+		libgcc #gcc compiler
+		android-studio #android tools		
+			#Languages
+			flutter #cross-platform app dev 
+
+
+		# CyberSec Tools
+		wireshark # packet sniffing
+
+		#Electrical Engineering Tools
+		kicad #pcb maker
+		logisim-evolution #pcb maker
+
+		# Art Tools
+		krita #picture manipulation tool
+		obs-studio #video recording software
+		handbrake #video compressor
+		curtail #image compressor
+		metadata-cleaner #cleans metadata
+		
 	];
 	# Sys Ver
 	system.stateVersion = "24.11";
@@ -136,5 +143,4 @@
 	services.blueman.enable = true;
 	hardware.bluetooth.enable = true; 
 	hardware.bluetooth.powerOnBoot = true;
-
 }
